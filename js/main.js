89027ba944c7485e3260fc2c2847eb685caf0e09
derekherbert <derek.herbert@hotmail.com>
2018-11-03 15:16:00 +1030

    Initial Commit

---
+++ js/main.js
@@ -0,0 +1,10 @@
+//Global Variables
+
+//Perform all initial tasks needed for launch
+function initialize() 
+{    
+    
+}
+
+//Call initialize method when window loads
+window.onload = function() { initialize(); };
